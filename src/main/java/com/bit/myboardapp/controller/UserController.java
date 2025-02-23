f853422f177716f8713d0875b575de5ace2a3ca1
DESKTOP-JSTJETN\COMTREE <bobaesj0923@gmail.com>
2025-02-23 16:16:03 +0900

    2025.02.23 feature/user 컨트롤러 예외처리 부분 수정

--- src/main/java/com/bit/myboardapp/controller/UserController.java
+++ src/main/java/com/bit/myboardapp/controller/UserController.java
@@ -29,19 +29,13 @@ public class UserController {
     public ResponseEntity<?> join(@RequestBody UserDto userDto) {
         ResponseDto<UserDto> responseDto = new ResponseDto<>();
 
-        try {
-            log.info("join userDto: {}", userDto);
-            UserDto joinUserDto = userService.join(userDto);
-            responseDto.setStatusCode(HttpStatus.CREATED.value());
-            responseDto.setStatusMessage("created");
-            responseDto.setItem(joinUserDto);
-            return ResponseEntity.ok(responseDto);
-        } catch (Exception e){
-            log.error("join error: {}", e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            responseDto.setStatusMessage(e.getMessage());
-            return ResponseEntity.internalServerError().body(responseDto);
-        }
+        log.info("join userDto: {}", userDto);
+
+        UserDto joinUserDto = userService.join(userDto);
+        responseDto.setStatusCode(HttpStatus.CREATED.value());
+        responseDto.setStatusMessage("created");
+        responseDto.setItem(joinUserDto);
+        return ResponseEntity.ok(responseDto);
     }
 
     // 로그인
@@ -49,19 +43,13 @@ public class UserController {
     public ResponseEntity<?> login(@RequestBody UserDto userDto) {
         ResponseDto<UserDto> responseDto = new ResponseDto<>();
 
-        try {
-            log.info("login userDto: {}", userDto);
-            UserDto LoginUserDto = userService.login(userDto);
-            responseDto.setStatusCode(HttpStatus.OK.value());
-            responseDto.setStatusMessage("ok");
-            responseDto.setItem(LoginUserDto);
-            return ResponseEntity.ok(responseDto);
-        } catch (Exception e) {
-            log.error("login error: {}", e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            responseDto.setStatusMessage(e.getMessage());
-            return ResponseEntity.internalServerError().body(responseDto);
-        }
+        log.info("login userDto: {}", userDto);
+
+        UserDto LoginUserDto = userService.login(userDto);
+        responseDto.setStatusCode(HttpStatus.OK.value());
+        responseDto.setStatusMessage("ok");
+        responseDto.setItem(LoginUserDto);
+        return ResponseEntity.ok(responseDto);
     }
 
     // 로그아웃
@@ -69,23 +57,18 @@ public class UserController {
     public ResponseEntity<?> logout(@RequestHeader("Authorization") String authHeader) {
         ResponseDto<Map<String, String>> responseDto = new ResponseDto<>();
 
-        try {
-            String token = authHeader.replace("Bearer ", "");
-            if(!jwtProvider.isValidToken(token)){
-                throw new IllegalArgumentException("invalid token");
-            }
-            Map<String, String> logoutMsgMap = new HashMap<>();
-            SecurityContext securityContext = SecurityContextHolder.getContext();
-            securityContext.setAuthentication(null);
-            SecurityContextHolder.setContext(securityContext);
-            logoutMsgMap.put("message", "logout successful");
-            return ResponseEntity.ok(logoutMsgMap);
-        } catch (Exception e){
-            log.error("logout error: {}", e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            responseDto.setStatusMessage(e.getMessage());
-            return ResponseEntity.internalServerError().body(responseDto);
+        String token = authHeader.replace("Bearer ", "");
+
+        if(!jwtProvider.isValidToken(token)){
+            throw new IllegalArgumentException("invalid token");
         }
+
+        Map<String, String> logoutMsgMap = new HashMap<>();
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(null);
+        SecurityContextHolder.setContext(securityContext);
+        logoutMsgMap.put("message", "logout successful");
+        return ResponseEntity.ok(logoutMsgMap);
     }
 
     // 이메일 중복 체크
@@ -93,39 +76,27 @@ public class UserController {
     public ResponseEntity<?> emailCheck(@RequestBody UserDto userDto) {
         ResponseDto<Map<String, String>> responseDto = new ResponseDto<>();
 
-        try {
-            log.info("emailCheck userDto: {}", userDto);
-            Map<String, String> emailCheckMap = userService.emailCheck(userDto);
-            responseDto.setStatusCode(HttpStatus.OK.value());
-            responseDto.setStatusMessage("email check successful");
-            responseDto.setItem(emailCheckMap);
-            return ResponseEntity.ok(responseDto);
-        } catch (Exception e){
-            log.error("emailCheck error: {}", e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            responseDto.setStatusMessage(e.getMessage());
-            return ResponseEntity.internalServerError().body(responseDto);
-        }
+        log.info("emailCheck userDto: {}", userDto);
+
+        Map<String, String> emailCheckMap = userService.emailCheck(userDto);
+        responseDto.setStatusCode(HttpStatus.OK.value());
+        responseDto.setStatusMessage("email check successful");
+        responseDto.setItem(emailCheckMap);
+        return ResponseEntity.ok(responseDto);
     }
 
     // 닉네임 중복 체크
     @GetMapping("/nickname-check")
-    public ResponseEntity<?> nicknameCheck(@RequestBody UserDto userDto) {
+    public ResponseEntity<?> nicknameCheck(@RequestBody UserDto userDto){
         ResponseDto<Map<String, String>> responseDto = new ResponseDto<>();
 
-        try {
-            log.info("nicknameCheck userDto: {}", userDto);
-            Map<String, String> nicknameCheckMap = userService.nicknameCheck(userDto);
-            responseDto.setStatusCode(HttpStatus.OK.value());
-            responseDto.setStatusMessage("nickname check successful");
-            responseDto.setItem(nicknameCheckMap);
-            return ResponseEntity.ok(responseDto);
-        } catch (Exception e) {
-            log.error("nicknameCheck error: {}", e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            responseDto.setStatusMessage(e.getMessage());
-            return ResponseEntity.internalServerError().body(responseDto);
-        }
+        log.info("nicknameCheck userDto: {}", userDto);
+
+        Map<String, String> nicknameCheckMap = userService.nicknameCheck(userDto);
+        responseDto.setStatusCode(HttpStatus.OK.value());
+        responseDto.setStatusMessage("nickname check successful");
+        responseDto.setItem(nicknameCheckMap);
+        return ResponseEntity.ok(responseDto);
     }
 
     // 전화번호
@@ -133,19 +104,13 @@ public class UserController {
     public ResponseEntity<?> telCheck(@RequestBody UserDto userDto) {
         ResponseDto<Map<String, String>> responseDto = new ResponseDto<>();
 
-        try {
-            log.info("telCheck userDto: {}", userDto);
-            Map<String, String> telcheckMap = userService.telCheck(userDto);
-            responseDto.setStatusCode(HttpStatus.OK.value());
-            responseDto.setStatusMessage("tel check successful");
-            responseDto.setItem(telcheckMap);
-            return ResponseEntity.ok(responseDto);
-        } catch (Exception e) {
-            log.error("telCheck error: {}", e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            responseDto.setStatusMessage(e.getMessage());
-            return ResponseEntity.internalServerError().body(responseDto);
-        }
+        log.info("telCheck userDto: {}", userDto);
+
+        Map<String, String> telcheckMap = userService.telCheck(userDto);
+        responseDto.setStatusCode(HttpStatus.OK.value());
+        responseDto.setStatusMessage("tel check successful");
+        responseDto.setItem(telcheckMap);
+        return ResponseEntity.ok(responseDto);
     }
 
     // 유저 정보 조회
@@ -153,19 +118,12 @@ public class UserController {
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
         ResponseDto<UserDto> responseDto = new ResponseDto<>();
 
-        try{
-            log.info("getUserById id: {}", id);
-            UserDto userInfo = userService.getUserById(id);
-            responseDto.setStatusCode(HttpStatus.OK.value());
-            responseDto.setStatusMessage("ok");
-            responseDto.setItem(userInfo);
-            return ResponseEntity.ok(responseDto);
-        } catch (Exception e){
-            log.error("getUserById error: {}", e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            responseDto.setStatusMessage(e.getMessage());
-            return ResponseEntity.internalServerError().body(responseDto);
-        }
-    }
+        log.info("getUserById id: {}", id);
 
+        UserDto userInfo = userService.getUserById(id);
+        responseDto.setStatusCode(HttpStatus.OK.value());
+        responseDto.setStatusMessage("ok");
+        responseDto.setItem(userInfo);
+        return ResponseEntity.ok(responseDto);
+    }
 }
